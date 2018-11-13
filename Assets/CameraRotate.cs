87c2f52aec371b99834cb21ee8906eb4f3d528d7
Dayason <TeamFiveAbertay@gmail.com>
2018-11-13 19:37:39 +0000

    Save

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, 45.0f * Time.deltaTime, Space.World);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, -45.0f * Time.deltaTime, Space.World);
+        }
+    }
+}
---
+++ Assets/SphereVisibility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereVisibility : MonoBehaviour {
+
+    public int id;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<MeshRenderer>().material = new Material(GetComponent<MeshRenderer>().material);
+        GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+
+        GameObject go = new GameObject("Number", typeof(TextMesh));
+        go.transform.position = transform.position + Vector3.up * 0.5f;
+        go.GetComponent<TextMesh>().text = id.ToString();
+        go.GetComponent<TextMesh>().fontSize = 500;
+        go.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+	}
+}
